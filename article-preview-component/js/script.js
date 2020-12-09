3c4fa33a723d1ee7f0d0d5ab95ef07af17a7e7fe
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2020-12-09 15:08:43 +0530

    File Uploads
    Added initial files

--- article-preview-component/js/script.js
+++ article-preview-component/js/script.js
@@ -2,8 +2,10 @@ const shareBtn = document.querySelector(".share-img-cont");
 const avatarDisp = document.querySelector(".avatar-disp");
 const shareDisp = document.querySelector(".share-disp");
 
-shareBtn.addEventListener("click", function () {
+function triggerEvent() {
 	avatarDisp.classList.toggle("avatar-hide");
 	shareDisp.classList.toggle("share-hide");
 	avatarDisp.parentElement.classList.toggle("bg-clr");
-});
+}
+
+shareBtn.addEventListener("click", triggerEvent);
