4939271cc5128be28c922ba0093e27e0e3e4fb5e
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-01-16 20:46:47 +0530

    Modified regex code

--- ping-coming-soon-page/js/scritp.js
+++ ping-coming-soon-page/js/scritp.js
@@ -3,13 +3,8 @@ const mail = document.querySelector("#email");
 const mssg = document.querySelector(".validate-mssg");
 
 form.addEventListener("submit", function (e) {
-	if (
-		/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-			mail.value
-		)
-	) {
-		return;
-	} else {
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!regex.test(mail.value)) {
 		e.preventDefault();
 		mssg.innerText = "Please provide a valid email address";
 		mail.classList.add("error");
