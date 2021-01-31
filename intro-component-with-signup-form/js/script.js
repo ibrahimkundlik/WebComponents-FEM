41a682042af4d27a93b1aee8a4527912a2e4f0dd
Ibrahim Kundlik <ibrahimkundlik@gmail.com>
2021-01-31 21:58:06 +0530

    Added validation and css for desktop layout

--- intro-component-with-signup-form/js/script.js
+++ intro-component-with-signup-form/js/script.js
@@ -1,12 +1,39 @@
-const trial = document.querySelector(".trial-btn");
-const error = document.querySelectorAll(".error-icon");
-const mssg = document.querySelectorAll(".error");
+const form = document.querySelector(".info-form");
+const mail = document.querySelector("#email");
 
-trial.addEventListener("click", () => {
-	error.forEach((curr) => {
-		curr.classList.add("error-show");
-	});
-	mssg.forEach((curr) => {
-		curr.classList.add("error-show");
-	});
+form.addEventListener("submit", function (e) {
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!regex.test(mail.value)) {
+		e.preventDefault();
+		showError(mail.nextElementSibling);
+		showError(mail.nextElementSibling.nextElementSibling);
+	} else {
+		removeError(mail.nextElementSibling);
+		removeError(mail.nextElementSibling.nextElementSibling);
+	}
+	ipCheck(e);
 });
+
+const ipCheck = (e) => {
+	for (let i = 0; i < 4; i++) {
+		if (i != 2) {
+			const ipVal = e.target.children[i].children[0].value;
+			if (ipVal.length === 0) {
+				e.preventDefault();
+				showError(e.target.children[i].children[1]);
+				showError(e.target.children[i].children[2]);
+			} else {
+				removeError(e.target.children[i].children[1]);
+				removeError(e.target.children[i].children[2]);
+			}
+		}
+	}
+};
+
+const showError = (target) => {
+	target.classList.add("error-show");
+};
+
+const removeError = (target) => {
+	target.classList.remove("error-show");
+};
